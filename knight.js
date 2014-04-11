9507d7b7dd93cea9dff9e59b9ae9402eca156e60
joe <joe@joeegan.co.uk>
2014-04-11 22:15:57 +0100

    add pony movement restrictions

--- knight.js
+++ knight.js
@@ -1,23 +1,30 @@
 (function(){
 
-'use strict';
+   'use strict';
 
-function Knight(colour){
-   this.colour = colour;
-   Knight.superclass.constructor.apply(this, arguments);
-}
-C.extend(Knight, C.Piece);
+   function Knight(colour){
+      this.colour = colour;
+      Knight.superclass.constructor.apply(this, arguments);
+   }
+   C.extend(Knight, C.Piece);
 
-Knight.prototype.colour = null;
+   Knight.prototype.checkLegal = function(selectedCoord, newCoord, turn, positions) {
+      var moveData = this._processMoveData(selectedCoord, newCoord, turn, positions);
+      var ALPHABET = C.Engine.ALPHABET;
+      var coordFileDifference = Math.abs(ALPHABET.indexOf(moveData.selectedCoordFile) - ALPHABET.indexOf(moveData.newCoordFile));
+      var coordRowDifference = Math.abs(moveData.selectedCoordRow - moveData.newCoordRow);
+      if (coordFileDifference == 1 && coordRowDifference == 2
+          || coordFileDifference == 2 && coordRowDifference == 1) {
+         console.log('you just moved like a pony');
+         return true;
+      }
+      return false;
+   };
 
-Knight.prototype.canMoveForwards = true;
+   Knight.prototype.BLACK_UNICODE = '\u265E';
 
-Knight.prototype.canMoveBackwards = false;
+   Knight.prototype.WHITE_UNICODE = '\u2658';
 
-Knight.prototype.BLACK_UNICODE = '\u265E';
-
-Knight.prototype.WHITE_UNICODE = '\u2658';
-
-C.Knight = Knight;
+   C.Knight = Knight;
 
 })();
