d205deabbcdddd088ec0cfca8a3bcce9daa96476
joe <joe@joeegan.co.uk>
2014-04-11 20:03:35 +0100

    store moveData for move validators

--- piece.js
+++ piece.js
@@ -10,36 +10,40 @@
    }
 
    Piece.prototype.checkLegal = function(selectedCoord, newCoord, turn) {
-      if ( (!this.canMoveBackwards && this.movedBackwards(arguments))
-         || (!this.canMultiMove && this.movedMultiMove(arguments))
-         || (!this.canMoveSideways && this.checkSideways(arguments))) {
+      var moveData = this._processMoveData(selectedCoord, newCoord, turn);
+      if ( (!this.canMoveBackwards && this.movedBackwards(moveData))
+         || (!this.canMultiMove && this.movedMultiMove(moveData))
+         || (!this.canMoveSideways && this.checkSideways(moveData))) {
          console.log('move was illegal');
          return false;
       }
       return true;
    };
+   
+   Piece.prototype._processMoveData = function(selectedCoord, newCoord, turn){
+     return {
+         selectedCoordRow: +selectedCoord.slice(1),
+         selectedCoordFile: selectedCoord.slice(0,1),
+         newCoordFile: newCoord.slice(0,1),
+         newCoordRow: +newCoord.slice(1),
+         turn: turn
+     }  
+   };
 
-   Piece.prototype.movedBackwards = function() {
-      var selectedCoordRow = +arguments[0][0].slice(1);
-      var newCoordRow = +arguments[0][1].slice(1);
-      var turn = arguments[0][2];
-      if ((turn == 'white' && newCoordRow < selectedCoordRow)
-      || (turn == 'black' && newCoordRow > selectedCoordRow)) {
+   Piece.prototype.movedBackwards = function(moveData) {
+      if ((moveData.turn == 'white' && moveData.newCoordRow < moveData.selectedCoordRow)
+      || (moveData.turn == 'black' && moveData.newCoordRow > moveData.selectedCoordRow)) {
          console.log('move was backwards');
          return true;
       }
       return false;
    };
 
-   Piece.prototype.movedMultiMove = function() {
+   Piece.prototype.movedMultiMove = function(moveData) {
       // temp, think of some OO way to reuse this...
-      var selectedCoordFile = arguments[0][0].slice(0,1);
-      var selectedCoordRow = +arguments[0][0].slice(1);
-      var newCoordFile = arguments[0][1].slice(0,1);
-      var newCoordRow = +arguments[0][1].slice(1);
       var ALPHABET = C.Engine.ALPHABET;
-      var coordFileDifference = Math.abs(ALPHABET.indexOf(selectedCoordFile) - ALPHABET.indexOf(newCoordFile));
-      var coordRowDifference = Math.abs(selectedCoordRow - newCoordRow);
+      var coordFileDifference = Math.abs(ALPHABET.indexOf(moveData.selectedCoordFile) - ALPHABET.indexOf(moveData.newCoordFile));
+      var coordRowDifference = Math.abs(moveData.selectedCoordRow - moveData.newCoordRow);
       if (coordFileDifference > 1
          || coordRowDifference > 1) {
          console.log('can\'t multimove');
@@ -48,10 +52,8 @@
       return false;
    };
 
-   Piece.prototype.checkSideways = function() {
-      var selectedCoordRow = +arguments[0][0].slice(1);
-      var newCoordRow = +arguments[0][1].slice(1);
-      if (selectedCoordRow == newCoordRow) {
+   Piece.prototype.checkSideways = function(moveData) {
+      if (moveData.selectedCoordRow == moveData.newCoordRow) {
          console.log('can\'t move sideways');
          return true;
       }
