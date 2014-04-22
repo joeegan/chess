6101c25077d2e22cb010573667f2ef3498c1a23b
joe <joe@joeegan.co.uk>
2014-04-22 21:26:10 +0100

    add travis integration

--- chess.js
+++ chess.js
@@ -30,6 +30,6 @@
    /**
     * Temporary! - To help with development drop in a moveLog here to reinitialise the game with a different position state.
     */
-   C.Chess.engine.processMoveLog(["1. Pe2-e4 Pe7-e5", "2. Pd2-d4 Pd7-d5", "3. Bc1-g5 Bf8-b4", "4. Bf1-b5 Bc8-g4", "5. Qd1-d2 Qd8-d7"]);
+//   C.Chess.engine.processMoveLog(["1. Pe2-e4 Pe7-e5", "2. Bf1-d3 Kb8-c6", "3. Pa2-a4 Pa7-a5", "4. Pb2-b4 Pa5xb4", "5. Pa4-a5 Kc6xa5", "6. Bd3-c4 Bf8-d6", "7. Kg1-f3 Kg8-h6", "8. Ke1-e2 Kh6-g4", "9. Ke2-d3 Pf7-f5", "10. Pe4xf5 Pe5-e4"]);
 
 })();
